24414cfe1467acc0aeaf6fbc0487368d00fdf5b7
delt-kz <yerkebulansarsenbek@yandex.kz>
2025-07-13 02:31:48 +0500

    Привести к CheckStyle

--- src/utils/TaskAccess.java
+++ src/utils/TaskAccess.java
@@ -6,10 +6,15 @@ import java.util.List;
 
 public interface TaskAccess<T> {
     T getById(int id);
+
     List<T> getAll();
+
     void delete(int id);
+
     void create(T task);
+
     void update(T task);
+
     default List<Subtask> getSubtasksOfEpic(int epicId) {
         throw new UnsupportedOperationException();
     }
